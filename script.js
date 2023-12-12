9a1d5c33687e29465341e6a6fa16b200a12d0d74
Hassan <104513483+hassan-980@users.noreply.github.com>
2023-12-12 23:59:00 +0530

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function openNav() {
     document.getElementById("nav").style.transition = "all 0.5s ease-out" ;
-    document.getElementById("nav").style.height = "275px";
+    document.getElementById("nav").style.height = "220px";
     document.getElementById("nav-btn").style.display = "none";
     document.getElementById("nav-cls").style.display = "block";
     document.getElementById("btn-nav").style.display = "block";
